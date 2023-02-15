f0c4ed12bd7fc1bd78913b1a71dce0c893f4b9f4
Sergei Knyazev <kniazevs.v@yandex.ru>
2023-02-15 19:12:42 +0300

    refactor: rewrote Sed as class instead, incapsulating the logic

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import re
 from dotenv import load_dotenv
 import os
 
-from sed import sed, sed_regex
+from sed import Sed
 
 load_dotenv()
 bot = telebot.TeleBot(os.getenv('API_TOKEN'))
@@ -20,8 +20,8 @@ Hi there, I am Jeaciaz's personal bot for stuff.
 Right now I only do message editing, to do so reply to a message with 's/thing/replacement/g', and I will replace the "thing" in message replied to with "replacement". I also support Python RegExp syntax, and you can use the regex flags after the last slash.
 """)
 
-@bot.message_handler(func=lambda message: re.match(sed_regex, message.text) is not None and message.reply_to_message is not None)
+@bot.message_handler(func=lambda message: Sed.is_valid(message))
 def echo_message(message):
-    bot.reply_to(message, sed(message))
+    bot.reply_to(message, Sed(message).calc())
 
 bot.infinity_polling()
--- sed.py
+++ sed.py
@@ -2,29 +2,37 @@ import re
 import functools
 
 sed_regex = r's/(.*?)/(.*?)/(.*)?'
+flag_replacements = {'g': 0, 'a': re.A, 'i': re.I, 'm': re.M, 's': re.S, 'x': re.X}
 
-def calc_flags(flags):
-  flag_replacements = {'g': 0, 'a': re.A, 'i': re.I, 'm': re.M, 's': re.S, 'x': re.X}
-  return functools.reduce(lambda acc, flag: acc | (flag_replacements[flag] or 0), flags, 0)
+class Sed:
+  """
+  Processes a message in the format of "s/thing/replacement/flags?", taking the message this one is a reply to and replacing "thing" with "replacement". Supports regex (python edition). Flags are standard regex flags: g, a, i, m, s, x are supported.
+  """
 
-def sed(message):
-  sed_help =  "To use sed, reply to a message with 's/thing/replacement/' to replace thing with replacement."
-  re_match = re.match(sed_regex, message.text)
+  @staticmethod
+  def is_valid(message):
+    """
+    Checks if message satisfies the requirements to be processed with sed, this includes format (s/thing/replacement/flags?) and a replied-to message.
+    """
+    return re.match(sed_regex, message.text) is not None and message.reply_to_message is not None
 
-  if message.reply_to_message is None or re_match is None:
-    bot.reply_to(message, sed_help)
-    return
+  def __init__(self, message):
+    """
+    Creates instance of Sed based on the message, validating it in the process with Sed.is_valid. If the message is invalid, throws ValueError.
+    """
+    if not Sed.is_valid(message):
+      raise ValueError('Invalid message format: should be a reply and be represented as "s/thing/replacement/[optional flags]".')
+    self.thing, self.replacement, self.flag_string = re.match(sed_regex, message.text).groups()
+    self.source_text = message.reply_to_message.text
 
-  thing, replacement, flags = re_match and re_match.groups()
-
-  count = 0 if 'g' in flags else 1
-  flag_num = calc_flags(flags)
-
-  try:
-    text = re.sub(thing, replacement, message.reply_to_message.text, count=count, flags=flag_num)
-    return text
-  except Exception as ex:
-    print(ex)
-    return f"""Cannot replace {thing} with {replacement}: {ex}.
-
-Try looking for an error in your input."""
+  def calc(self):
+    """
+    Returns the edited original message's replied-to message.
+    """
+    flags = functools.reduce(lambda acc, flag: acc | (flag_replacements[flag] or 0), self.flag_string, 0)
+    count = 0 if 'g' in self.flag_string else 1
+    try:
+      return re.sub(self.thing, self.replacement, self.source_text, count=count, flags=flags)
+    except Exception as ex:
+      print(ex)
+      return f"Cannot replace {self.thing} with {self.replacement}: {ex}.\n\nTry looking for an error in your input."
